35fef93be4b947a8926074d7f433687763794ce1
aanchalthakur28 <athakur28@myseneca.ca>
2023-12-11 19:33:45 +0000

    Added config and httpd.ssh

---
+++ Prod/Webserver/config.tf
@@ -0,0 +1,7 @@
+terraform  {
+  backend "s3" {
+    bucket = "acs730-group6-s3bucket"            // Bucket from where to GET Terraform State
+    key    = "prod/webserver/terraform.tfstate" // Object name in the bucket to GET Terraform State
+    region = "us-east-1"                       // Region where bucket created
+  }
+}
