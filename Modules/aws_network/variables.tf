d5043f98aa03ff5e6e33d457455c0cb77c04feae
“npaul1909” <npaul19@myseneca.ca>
2023-12-12 18:50:59 +0000

    Updated descriptions

--- Modules/aws_network/variables.tf
+++ Modules/aws_network/variables.tf
@@ -19,13 +19,13 @@ variable "env" {
 variable "private_subnet_cidrs" {
   default     = ["10.1.5.0/24", "10.1.6.0/24"]
   type        = list(string)
-  description = "Private Subnet CIDRs"
+  description = "Private Subnet"
 }
 
 variable "public_cidr_blocks" {
   default     = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24", "10.1.4.0/24"]
   type        = list(string)
-  description = "Public Subnet CIDRs"
+  description = "Public Subnet"
 }
 
 variable "default_tags" {
